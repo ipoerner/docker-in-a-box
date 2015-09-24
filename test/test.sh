0bea54a8cb7532aaaae709dc3646152b885ff1a2
Ingmar Pörner <ingmar.poerner@rhebo.com>
2015-09-25 00:50:45 +0200

    Remove now obsolete /test directory.

--- test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-vagrant destroy --force
-vagrant box add --name docker-in-a-box ../vagrant-box-builder/output/ubuntu-server-amd64-docker.box
-vagrant up
-vagrant destroy --force
-vagrant box remove docker-in-a-box
