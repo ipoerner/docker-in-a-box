c9f07220aa77eae05947b223f88aaf28544c95e8
Ingmar Pörner <ingmar.poerner@rhebo.com>
2015-09-25 00:50:11 +0200

    Refactoring
    Reformat indent of 'docker' call.

--- box-it-up.sh
+++ box-it-up.sh
@@ -5,7 +5,10 @@ set -e
 container_cleanup() {
     container_id=$(docker ps --all --quiet --filter "name=${1}")
     if ! [ -z "${container_id}" ]; then
-        docker rm --force --volumes ${container_id}
+        docker rm \
+            --force \
+            --volumes \
+            ${container_id}
     fi
 }
 
